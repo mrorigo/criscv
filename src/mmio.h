762f92ab870002d58804878f19bcde490666c0d8
Mattias Nilsson <mattias.nilsson@nshift.com>
2022-05-01 10:47:40 +0200

    Added mmio_perm_t, for future use

--- src/mmio.h
+++ src/mmio.h
@@ -7,12 +7,19 @@ typedef enum __attribute((packed)) _memory_access_width_t {
   BYTE, HALFWORD, WORD
 } memory_access_width_t;
 
+typedef enum _mmio_perm_t {
+  READ	= 1<<0,
+  WRITE	= 1<<1,
+  EXEC	= 1<<2
+} mmio_perm_t;
+
 typedef struct _mmio_device_t {
   struct _mmio_device_t *next;
 
   uint32_t base_address; 
   uint32_t size; // in 32bit words
   void    *user;
+  mmio_perm_t perm;
 
   void     (*init )(struct _mmio_device_t *device);
   uint32_t (*read )(const struct _mmio_device_t *device, const uint32_t offs, const memory_access_width_t aw);
