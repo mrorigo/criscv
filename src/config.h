df82348de965e912f4ab0f529c16eb78351bebda
Mattias Nilsson <mattias.nilsson@nshift.com>
2022-05-01 10:47:21 +0200

    Moved config constants to config.h

--- src/config.h
+++ src/config.h
@@ -1,8 +1,18 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define PREFETCH_SIZE 2
-#define NUMCORES 1
+#define PREFETCH_SIZE 4
+#define NUMCORES 2
+
+#define RAM_SIZE 1024 * 1000 * 4 // 4mb
+#define ROM_SIZE 1024            // 1kb
+
+#define ROM_START 0x10000000
+#define RAM_START 0x20000000
+
+#define STACK_SIZE (1<<12)  // 16kb is enough for everyone
+
+#define STACK_TOP ((RAM_START + RAM_SIZE) - (STACK_SIZE))
 
 
 #endif
