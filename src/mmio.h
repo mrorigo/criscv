ad773091063bf87ce26852f5c4c64a449736bd8b
Mattias Nilsson <mattias.nilsson@nshift.com>
2022-05-21 14:33:52 +0200

    mmio: now possible to issue multi-element bus requests

--- src/mmio.h
+++ src/mmio.h
@@ -9,9 +9,14 @@ typedef enum __attribute((packed)) _memory_access_width_t {
 
 typedef enum _mmio_perm_t {
   READ	= 1<<0,
-  WRITE	= 1<<1,
-  EXEC	= 1<<2
+  WRITE	= 1<<1
 } mmio_perm_t;
+
+typedef enum _device_state_t {
+  READY,
+  BUSY,
+  ERROR
+} device_state_t;
 
 typedef struct _mmio_device_t {
   struct _mmio_device_t *next;
@@ -20,11 +25,30 @@ typedef struct _mmio_device_t {
   uint32_t size; // in 32bit words
   void    *user;
   mmio_perm_t perm;
-
+  device_state_t state;
+      
   void     (*init )(struct _mmio_device_t *device);
-  uint32_t (*read_single )(const struct _mmio_device_t *device, const uint32_t offs, const memory_access_width_t aw);
-  size_t   (*read )(const struct _mmio_device_t *device, void *buf, const uint32_t offs, size_t size, const memory_access_width_t aw);
-  void     (*write)(struct _mmio_device_t *device, const uint32_t offs, const uint32_t value, const memory_access_width_t aw);
+  uint32_t (*read_single)(struct _mmio_device_t *device,
+			   const uint32_t offs,
+			   const memory_access_width_t aw);
+
+  size_t   (*read)(struct _mmio_device_t *device,
+		   const uint32_t offs,
+		   void *buf,
+		   const size_t size,
+		   const memory_access_width_t aw);
+  
+
+  void   (*write_single)(struct _mmio_device_t *device,
+			   const uint32_t offs,
+			   const uint32_t value,
+			   const memory_access_width_t aw);
+
+  size_t     (*write)(struct _mmio_device_t *device,
+		    const uint32_t offs,
+		    const void *buf,
+		    size_t count,
+		    const memory_access_width_t aw);
 
 } mmio_device_t;
 
